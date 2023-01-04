7b47057af558f24c216c752d106aa3699720de29
mariacjones1 <mariacjones1@hotmail.co.uk>
2023-01-04 12:34:25 +0000

    Add subtraction game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,6 +29,8 @@ function runGame(gameType) {
 
     if (gameType === "add") {
         displayAddQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
     } else {
@@ -68,6 +70,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "add"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
     } else {
@@ -98,8 +102,10 @@ function displayAddQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "+";
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
